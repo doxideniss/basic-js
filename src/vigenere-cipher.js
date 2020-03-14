edf5e3ed9d130501862741ed27062a3033d2213a
Deniss <doxideniss@gmail.com>
2020-03-14 22:41:05 +0200

    feat: add all complete tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,72 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(isDirect) {
+        this.isDirect = isDirect === undefined ? true : isDirect;
+        this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message|| !key) {
+            throw Error;
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let result = "";
+        for (let i = 0; i < message.length; i++) {
+            if (this.alphabet.includes(message.charAt(i))) {
+                result += this.alphabet.charAt(
+                  (this.alphabet.indexOf(message.charAt(i)) +
+                    this.alphabet.indexOf(key.charAt(i % key.length))) %
+                  this.alphabet.length
+                );
+            } else {
+                result += message.charAt(i);
+                message = message.slice(0, i) + message.slice(i + 1);
+                i--;
+            }
+        }
+        result = this.isDirect
+          ? result
+          : result
+            .split("")
+            .reverse()
+            .join("");
+
+        return result;
+    }
+
+    decrypt(message, key) {
+        if (!message|| !key) {
+            throw Error;
+        }
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        let result = "";
+        for (let i = 0; i < message.length; i++) {
+            if (this.alphabet.includes(message.charAt(i))) {
+                result += this.alphabet.charAt(
+                  (this.alphabet.indexOf(message.charAt(i)) +
+                    this.alphabet.length -
+                    this.alphabet.indexOf(key.charAt(i % key.length))) %
+                  this.alphabet.length
+                );
+            } else {
+                result += message.charAt(i);
+                message = message.slice(0, i) + message.slice(i + 1);
+                i--;
+            }
+        }
+
+        result = this.isDirect
+          ? result
+          : result
+            .split("")
+            .reverse()
+            .join("");
+
+        return result;
     }
 }
 
